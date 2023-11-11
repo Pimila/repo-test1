91aad4cfcab17f53f0e1b8d128acfa4f17afd23f
Pimila <128486040+Pimila@users.noreply.github.com>
2023-11-11 18:59:03 +0200

    add src folder in repo

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise002
+{
+  using System;
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+       string lyrics = "Once upon a time\nI was falling in love\nNow I'm only falling apart";
+        {
+            Console.WriteLine(lyrics);
+            Console.WriteLine(lyrics);
+            Console.WriteLine(lyrics);
+        }
+    }
+  }
+}
+
+/*
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        string lyrics = "Once upon a time\nI was falling in love\nNow I'm only falling apart";
+        for (int i = 0; i < 3; i++) {
+            Console.WriteLine(lyrics);
+        }
+    }
+}
+
+*/
